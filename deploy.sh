6d956de08880c74c677de92f2c37e9a8e5a4fb79
Douglas Landvik <douglas.landvik@gmail.com>
2024-04-22 10:57:59 +0200

    fix(store): return supply duration on getStoreItems

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+#
+## Ensure the builder is running
+#docker buildx inspect --bootstrap
+#
+## Build the Docker image with buildx specifying the platform
+#docker buildx build --platform linux/amd64 -t gcr.io/vetai1994/pythonserver:latest --push .
+#
+## Push the Docker image to GCR (this step can be omitted if included in the buildx command with --push)
+#docker push gcr.io/vetai1994/pythonserver:latest
+#
+## Deploy the image to Cloud Run
+#gcloud run deploy pythonserver --image gcr.io/vetai1994/pythonserver --platform managed --region europe-west2
 
-# Ensure the builder is running
-docker buildx inspect --bootstrap
-
-# Build the Docker image with buildx specifying the platform
-docker buildx build --platform linux/amd64 -t gcr.io/vetai1994/pythonserver:latest --push .
-
-# Push the Docker image to GCR (this step can be omitted if included in the buildx command with --push)
-docker push gcr.io/vetai1994/pythonserver:latest
-
-# Deploy the image to Cloud Run
-gcloud run deploy pythonserver --image gcr.io/vetai1994/pythonserver --platform managed --region europe-west2
+for i in {1..3}; do
+  eval "echo Loop iteration  $i" 
+done

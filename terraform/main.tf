35b785955ee40f4f31f065c7466d22435349c2fe
Douglas Landvik <douglas.landvik@gmail.com>
2024-04-23 11:07:09 +0200

    fix(store): return supply duration on getStoreItems

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,10 +23,10 @@ locals {
 		"artifact"             = "artifactregistry.googleapis.com",
 	}
 	bindings = {
-#		"roles/iam.serviceAccountTokenCreator" = "serviceAccount:${google_service_account.service_account.email}"
-#		"roles/iam.serviceAccountUser"         = "serviceAccount:${google_service_account.service_account.email}"
-#		"roles/artifactregistry.writer"        = "serviceAccount:${google_service_account.service_account.email}"
-		"roles/editor"                         = "serviceAccount:${google_service_account.service_account.email}"
+		"roles/iam.serviceAccountTokenCreator" = "serviceAccount:${google_service_account.service_account.email}"
+		"roles/iam.serviceAccountUser"         = "serviceAccount:${google_service_account.service_account.email}"
+		"roles/artifactregistry.writer"        = "serviceAccount:${google_service_account.service_account.email}"
+#		"roles/editor"                         = "serviceAccount:${google_service_account.service_account.email}"
 	}
 }
 

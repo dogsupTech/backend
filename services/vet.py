53cdc0291587ce7b33aa335a3d8747779529fe30
Douglas Landvik <douglas.landvik@gmail.com>
2024-07-23 11:33:50 +0200

    fix(leaderboard): return nil when leaderboard is empty

--- services/vet.py
+++ services/vet.py
@@ -1,5 +1,5 @@
 # services/vet.py
-from firebase_admin import firestore
+from firebase_admin import firestore, auth
 from uuid import uuid4
 
 class VetService:
@@ -15,4 +15,13 @@ class VetService:
             "specialization": data.get("specialization")
         }
         db.collection('clinics').document(clinic_id).collection('vets').document(vet_id).set(new_vet)
+        
+        # Create a new user in Firebase Authentication
+        user = auth.create_user(
+            email=data.get("email"),
+            email_verified=False,
+            password=data.get("password"),  # Ensure password is in the data
+            display_name=data.get("name"),
+            disabled=False
+        )
         return vet_id
